4deb43f094ba16647a9e01b87ad1831bfc8c0d92
gabmfernandes <gabrielmfer127@gmail.com>
2025-10-25 16:41:05 -0300

    :sparkles: feat: adicionar serializer de User

--- backend/filmes_favoritos_api/serializers.py
+++ backend/filmes_favoritos_api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import FavoriteMovie, ShareableList
+from django.contrib.auth.models import User
 
 class FavoriteMovieSerializer(serializers.ModelSerializer):
     """
@@ -32,3 +33,20 @@ class ShareableListSerializer(serializers.ModelSerializer):
         model = ShareableList
         fields = ['share_hash', 'favorites', 'created_at']
         read_only_fields = ('share_hash', 'created_at')
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer para registro de novo usuário."""
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'email': {'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
