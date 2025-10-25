cd0eff9ceb265b2307ceb44902113b1e3ed6beed
gabmfernandes <gabrielmfer127@gmail.com>
2025-10-25 13:56:01 -0300

    :sparkles: feat: adicionar urls de gerenciamento e remoção de filmes favoritos

--- backend/filmes_favoritos_api/urls.py
+++ backend/filmes_favoritos_api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import MovieSearchView
-# FavoriteListCreateView, FavoriteDestroyView 
-# Certifique-se de importar todas as Views, incluindo MovieSearchView
+from .views import MovieSearchView, FavoriteListCreateView, FavoriteDestroyView
 
 urlpatterns = [
     # Endpoint de Pesquisa
     path('search/', MovieSearchView.as_view(), name='movie-search'),
+
+    # Gerenciamento da Lista (GET e POST)
+    path('favorites/', FavoriteListCreateView.as_view(), name='favorite-list-create'),
+    
+    # Remoção (DELETE)
+    path('favorites/<int:tmdb_id>/', FavoriteDestroyView.as_view(), name='favorite-destroy'),
 ]
